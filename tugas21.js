d5cf17c046e792fc36670783b32d24690654fd99
jauhhari <ervanadiwijaya@gmail.com>
2021-07-01 21:47:16 +0700

    OK

---
+++ tugas21.js
@@ -0,0 +1,9 @@
+function change() {
+    var kal = "Saya belajar masak nasi goreng";
+    var hasil = kal.replace("masak", "makan");
+
+    console.log(kal);
+    console.log(hasil);
+}
+
+change();
